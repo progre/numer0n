2e1fa642c54bd7f566b9304b6a62e91d649a740b
progre <djyayutto@gmail.com>
2013-11-11 22:36:52 +0900

    fix

--- src/public/javascript/main.ts
+++ src/public/javascript/main.ts
@@ -14,82 +14,82 @@ app.config(['$routeProvider',
     }
 ]);
 app.controller('IndexController',
-    ['$scope', '$timeout', ($scope: ng.IScope, $timeout: ng.ITimeoutService) => {
+    ['$scope', '$timeout', ($scope: any, $timeout: ng.ITimeoutService) => {
         var numer0n = new Numer0n();
-        $scope['aiNumer'] = numer0n.getAINumber();
-        $scope['start'] = (playerNumber: string, playerFirst: boolean) => {
-            $scope['danger'] = '';
+        $scope.aiNumer = numer0n.getAINumber();
+        $scope.start = (playerNumber: string, playerFirst: boolean) => {
+            $scope.danger = '';
             try {
-                numer0n.start($scope['name'], playerNumber, playerFirst);
+                numer0n.start($scope.name, playerNumber, playerFirst);
             } catch (e) {
-                $scope['danger'] = e.message;
+                $scope.danger = e.message;
                 return;
             }
-            $scope['isGameStarted'] = true;
+            $scope.isGameStarted = true;
             mainLoop($timeout, $scope, numer0n);
         };
-        $scope['call'] = () => {
-            $scope['danger'] = '';
+        $scope.call = () => {
+            $scope.danger = '';
             try {
-                numer0n.put($scope['lastPlayerAttack']);
+                numer0n.put($scope.lastPlayerAttack);
             } catch (e) {
-                $scope['danger'] = e.message;
+                $scope.danger = e.message;
                 return;
             }
             mainLoop($timeout, $scope, numer0n);
         };
-        $scope['next'] = () => {
-            $scope['danger'] = '';
+        $scope.next = () => {
+            $scope.danger = '';
             mainLoop($timeout, $scope, numer0n);
         };
-        $scope['reset'] = () => {
-            $scope['wait'] = false;
-            $scope['done'] = false;
-            $scope['isGameStarted'] = false;
-            $scope['info'] = '';
-            $scope['lastPlayerAttack'] = '';
-            $scope['playerMessage'] = '';
-            $scope['lastAIAttack'] = '';
-            $scope['aiMessage'] = '';
-            $scope['playerAttacks'] = [];
-            $scope['aiAttacks'] = [];
+        $scope.reset = () => {
+            $scope.wait = false;
+            $scope.done = false;
+            $scope.isGameStarted = false;
+            $scope.info = '';
+            $scope.lastPlayerAttack = '';
+            $scope.playerMessage = '';
+            $scope.lastAIAttack = '';
+            $scope.aiMessage = '';
+            $scope.playerAttacks = [];
+            $scope.aiAttacks = [];
             numer0n = new Numer0n();
-            $scope['aiNumer'] = numer0n.getAINumber();
+            $scope.aiNumer = numer0n.getAINumber();
         };
     }]);
 
 angular.bootstrap(<any>document, ['app']);
 
-function mainLoop($timeout: ng.ITimeoutService, $scope: ng.IScope, numer0n: Numer0n) {
-    $scope['wait'] = true;
+function mainLoop($timeout: ng.ITimeoutService, $scope: any, numer0n: Numer0n) {
+    $scope.wait = true;
     numer0n.nextAsync().then(sleep => $scope.$apply($scope => {
-        $scope['info'] = numer0n.info;
-        $scope['lastPlayerAttack'] = numer0n.lastPlayerAttack;
-        $scope['playerMessage'] = numer0n.playerMessage;
-        $scope['lastAIAttack'] = numer0n.lastAIAttack;
-        $scope['aiMessage'] = numer0n.aiMessage;
-        $scope['playerAttacks'] = numer0n.playerAttacks;
-        $scope['aiAttacks'] = numer0n.aiAttacks;
+        $scope.info = numer0n.info;
+        $scope.lastPlayerAttack = numer0n.lastPlayerAttack;
+        $scope.playerMessage = numer0n.playerMessage;
+        $scope.lastAIAttack = numer0n.lastAIAttack;
+        $scope.aiMessage = numer0n.aiMessage;
+        $scope.playerAttacks = numer0n.playerAttacks;
+        $scope.aiAttacks = numer0n.aiAttacks;
         switch (sleep) {
             case -1:
-                $scope['isPlayerTurn'] = true;
-                $scope['wait'] = false;
+                $scope.isPlayerTurn = true;
+                $scope.wait = false;
                 return;
             case -3:
-                $scope['isPlayerTurn'] = false;
-                $scope['wait'] = false;
+                $scope.isPlayerTurn = false;
+                $scope.wait = false;
                 return;
             case -2:
-                $scope['done'] = true;
+                $scope.done = true;
                 return;
         }
         $timeout(() => mainLoop($timeout, $scope, numer0n), sleep);
     }));
-    $scope['info'] = numer0n.info;
-    $scope['lastPlayerAttack'] = numer0n.lastPlayerAttack;
-    $scope['playerMessage'] = numer0n.playerMessage;
-    $scope['lastAIAttack'] = numer0n.lastAIAttack;
-    $scope['aiMessage'] = numer0n.aiMessage;
-    $scope['playerAttacks'] = numer0n.playerAttacks;
-    $scope['aiAttacks'] = numer0n.aiAttacks;
+    $scope.info = numer0n.info;
+    $scope.lastPlayerAttack = numer0n.lastPlayerAttack;
+    $scope.playerMessage = numer0n.playerMessage;
+    $scope.lastAIAttack = numer0n.lastAIAttack;
+    $scope.aiMessage = numer0n.aiMessage;
+    $scope.playerAttacks = numer0n.playerAttacks;
+    $scope.aiAttacks = numer0n.aiAttacks;
 }
